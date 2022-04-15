5d9cb0b7fa76da89ca076c67e0615b14835021c4
Georgi Sabev <georgethebeatle@gmail.com>
2022-04-15 15:13:47 +0300

    Fix repo name
    There is no korifi-controllers repo. Probably a rename mistake
    
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- pipelines/scripts/allow-e2e.sh
+++ pipelines/scripts/allow-e2e.sh
@@ -7,11 +7,11 @@ PR_AUTHOR=$(<pr-label/.git/resource/author)
 
 gh auth login --with-token <<<$KORIFI_BOT_TOKEN
 
-team=$(gh repo view cloudfoundry/korifi-controllers --json assignableUsers --jq '.assignableUsers[].login')
+team=$(gh repo view cloudfoundry/korifi --json assignableUsers --jq '.assignableUsers[].login')
 
 for user in $team; do
   if [[ "$user" == "$PR_AUTHOR" ]]; then
     echo Allowing e2e run for team member "$user"
-    gh pr --repo cloudfoundry/korifi-controllers edit $PR_NUMBER --add-label "e2e-allowed"
+    gh pr --repo cloudfoundry/korifi edit $PR_NUMBER --add-label "e2e-allowed"
   fi
 done
