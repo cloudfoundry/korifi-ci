2d752233d51ef7425cd921d3697dbe7df418d2fb
Danail Branekov <danailster@gmail.com>
2022-06-01 11:10:41 +0000

    Publish code coverage to codeclimate
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

---
+++ pipelines/scripts/publish-code-coverage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export GIT_COMMIT_SHA=$(<korifi/.git/ref)
+export GIT_BRANCH=main
+
+pushd coverage
+{
+  cc-test-reporter before-build
+  cc-test-reporter sum-coverage *.codeclimate.json -p $(ls *.codeclimate.json | wc -l) -o coverage.total.json
+  cc-test-reporter upload-coverage -i coverage.total.json
+}
+popd
--- pipelines/scripts/run-make-target.sh
+++ pipelines/scripts/run-make-target.sh
@@ -3,3 +3,9 @@
 set -euo pipefail
 
 make -C "root/$DIR" "$TARGET"
+
+idx=0
+for c in $(find "root/$DIR" -name cover.out); do
+  cc-test-reporter format-coverage $c -t gocov -o coverage/$TARGET-$idx.codeclimate.json -p $(dirname $c)
+  idx=$((idx + 1))
+done
