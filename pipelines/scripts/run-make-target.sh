d83e3df4fc489696ea1b29c9d9c5588d8023ecc4
Danail Branekov <danailster@gmail.com>
2022-06-01 11:19:47 +0000

    Attempt to fix format-coverage invocation
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- pipelines/scripts/run-make-target.sh
+++ pipelines/scripts/run-make-target.sh
@@ -4,8 +4,12 @@ set -euo pipefail
 
 make -C "root/$DIR" "$TARGET"
 
-idx=0
-for c in $(find "root/$DIR" -name cover.out); do
-  cc-test-reporter format-coverage $c -t gocov -o coverage/$TARGET-$idx.codeclimate.json -p $(dirname $c)
-  idx=$((idx + 1))
-done
+pushd root
+{
+  idx=0
+  for c in $(find "$DIR" -name cover.out); do
+    cc-test-reporter format-coverage $c -t gocov -o ../coverage/$TARGET-$idx.codeclimate.json -p $(dirname $c)
+    idx=$((idx + 1))
+  done
+}
+popd
