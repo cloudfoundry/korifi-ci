428bf726c96992cf84750bf7ca907e3b170f8a3a
Kieron Browne <kbrowne@vmware.com>
2022-06-08 12:29:38 +0100

    Redo the clean-up image script
    We should be calling `gcloud artifacts _docker_ ...` so take advantage
    of that!
    
    This means we get timestamps!

--- pipelines/scripts/clean-gcp-artifact-registry.sh
+++ pipelines/scripts/clean-gcp-artifact-registry.sh
@@ -7,20 +7,33 @@ source korifi-ci/pipelines/scripts/common/gcloud-functions
 tmp="$(mktemp -d)"
 trap "rm -rf $tmp" EXIT
 
-gcloud() {
-  $(which gcloud) --project=cf-on-k8s-wg $@
+gcloudx() {
+  gcloud --project=${PROJECT} $@
 }
 
 main() {
   gcloud-login
 
-  export -f gcloud
+  export -f gcloudx
 
-  # delete all images produced by kpack during the tests
-  gcloud artifacts packages list --repository=$KPACK_REPO_NAME --location=$KPACK_REPO_LOCATION --format=json --filter='name!~^kpack/beta$' |
-    jq '.[]|.name' -r |
-    sed 's|/|%2F|g' |
-    xargs -IN gcloud artifacts packages delete N --quiet --repository=$KPACK_REPO_NAME --location=$KPACK_REPO_LOCATION --async
+  # delete all images produced by kpack during the tests - but not the kpack cluster-builder image
+  gcloudx artifacts docker images list ${KPACK_REPO_LOCATION}-docker.pkg.dev/${PROJECT}/${KPACK_REPO_NAME} --format=json --filter="package!~/kpack/beta\$" |
+    jq -r '.[]|.package + "@" + .version' |
+    xargs -I {} bash -c 'gcloudx "$@"' _ artifacts docker images delete {} --async --quiet --delete-tags
+
+  # delete tags of korifi images older than a day
+  yesterday=$(date -d "yesterday" -Iseconds -u)
+  for package in korifi-api korifi-controllers korifi-kpack-image-builder; do
+    gcloudx artifacts docker images list ${CI_REPO_LOCATION}-docker.pkg.dev/${PROJECT}/${CI_REPO_NAME}/${package} --format=json --filter="createTime<${yesterday}" |
+      jq -r '.[]|.package + "@" + .version' |
+      xargs -I {} bash -c 'gcloudx "$@"' _ artifacts docker images delete {} --async --quiet --delete-tags
+  done
+
+  echo "################# REPOSITORY SIZES #################"
+  gcloud artifacts repositories describe --location ${KPACK_REPO_LOCATION} ${KPACK_REPO_NAME}
+  echo
+  gcloud artifacts repositories describe --location ${CI_REPO_LOCATION} ${CI_REPO_NAME}
+  echo "####################################################"
 }
 
 main
