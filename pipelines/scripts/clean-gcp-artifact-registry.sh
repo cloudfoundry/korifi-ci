54b97732b57ea553c517ab03e777169e3f95171e
Kieron Browne <kbrowne@vmware.com>
2022-06-08 14:08:54 +0100

    Delete full images from pr-e2e-images
    We don't need to delete version by version. Do the whole lot at once

--- pipelines/scripts/clean-gcp-artifact-registry.sh
+++ pipelines/scripts/clean-gcp-artifact-registry.sh
@@ -18,10 +18,10 @@ main() {
 
   # delete all images produced by kpack during the tests - but not the kpack cluster-builder image
   gcloudx artifacts docker images list ${KPACK_REPO_LOCATION}-docker.pkg.dev/${PROJECT}/${KPACK_REPO_NAME} --format=json --filter="package!~/kpack/beta\$" |
-    jq -r '.[]|.package + "@" + .version' |
-    xargs -I {} bash -c 'gcloudx "$@"' _ artifacts docker images delete {} --async --quiet --delete-tags
+    jq -r '.[]|.package' |
+    xargs -I {} bash -c 'gcloudx "$@"' _ artifacts docker images delete {} --async --quiet
 
-  # delete tags of korifi images older than a day
+  # delete versions of korifi images older than a day
   yesterday=$(date -d "yesterday" -Iseconds -u)
   for package in korifi-api korifi-controllers korifi-kpack-image-builder; do
     gcloudx artifacts docker images list ${CI_REPO_LOCATION}-docker.pkg.dev/${PROJECT}/${CI_REPO_NAME}/${package} --format=json --filter="createTime<${yesterday}" |
