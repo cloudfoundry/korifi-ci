3c88d81f084631a8cbd876e31fda48e8ceda9911
Kieron Browne <kbrowne@vmware.com>
2022-06-16 09:58:44 +0000

    Try again turning cert contents into files
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- pipelines/scripts/run-smoke-tests.sh
+++ pipelines/scripts/run-smoke-tests.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 KUBECONFIG="$(realpath "$KUBECONFIG")"
 
-kubectl config set-credentials "${SMOKE_TEST_USER}" --client-certificate="<(echo $CF_ADMIN_CERT)" --client-key="<(echo $CF_ADMIN_KEY)" --embed-certs
+kubectl config set-credentials "${SMOKE_TEST_USER}" --client-certificate=<(echo "$CF_ADMIN_CERT") --client-key=<(echo "$CF_ADMIN_KEY") --embed-certs
 
 cd korifi/tests/smoke
 ginkgo
