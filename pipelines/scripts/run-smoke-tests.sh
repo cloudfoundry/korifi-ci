21da9e561bb18613907dbfe221f4d07a5c50eb28
Kieron Browne <kbrowne@vmware.com>
2022-06-16 10:12:34 +0000

    base64 -d correctly
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- pipelines/scripts/run-smoke-tests.sh
+++ pipelines/scripts/run-smoke-tests.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 KUBECONFIG="$(realpath "$KUBECONFIG")"
 
-kubectl config set-credentials "${SMOKE_TEST_USER}" --client-certificate=<(base64 -d "$CF_ADMIN_CERT") --client-key=<(base64 -d "$CF_ADMIN_KEY") --embed-certs
+kubectl config set-credentials "${SMOKE_TEST_USER}" --client-certificate=<(base64 -d <<<"$CF_ADMIN_CERT") --client-key=<(base64 -d <<<"$CF_ADMIN_KEY") --embed-certs
 
 cd korifi/tests/smoke
 ginkgo
