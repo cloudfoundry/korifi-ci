64b13104ffb0ce9474ed35315571bd49fb2f227e
Georgi Sabev <georgethebeatle@gmail.com>
2022-07-14 13:09:13 +0000

    Need to build within the korifi dir
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- pipelines/scripts/deploy-korifi.sh
+++ pipelines/scripts/deploy-korifi.sh
@@ -22,17 +22,22 @@ generate_kube_config() {
 }
 
 deploy_cf() {
-  kubectl kustomize "korifi-ci/build/overlays/controllers/$CLUSTER_NAME" | kbld -f "korifi-ci/build/kbld/$CLUSTER_NAME/korifi-controllers-kbld.yml" -f- | kapp deploy -y -a korifi-controllers -f-
-  create_tls_secret "korifi-workloads-ingress-cert" "korifi-controllers-system" "*.$CLUSTER_NAME.korifi.cf-app.com"
+  pushd korifi
+  {
+    kubectl kustomize "../korifi-ci/build/overlays/controllers/$CLUSTER_NAME" | kbld -f "../korifi-ci/build/kbld/$CLUSTER_NAME/korifi-controllers-kbld.yml" -f- | kapp deploy -y -a korifi-controllers -f-
+    create_tls_secret "korifi-workloads-ingress-cert" "korifi-controllers-system" "*.$CLUSTER_NAME.korifi.cf-app.com"
 
-  kubectl kustomize "korifi-ci/build/overlays/api/$CLUSTER_NAME" | kbld -f "korifi-ci/build/kbld/$CLUSTER_NAME/korifi-api-kbld.yml" -f- | kapp deploy -y -a korifi-api -f-
-  create_tls_secret "korifi-api-ingress-cert" "korifi-api-system" "*.$CLUSTER_NAME.korifi.cf-app.com"
+    kubectl kustomize "../korifi-ci/build/overlays/api/$CLUSTER_NAME" | kbld -f "../korifi-ci/build/kbld/$CLUSTER_NAME/korifi-api-kbld.yml" -f- | kapp deploy -y -a korifi-api -f-
+    create_tls_secret "korifi-api-ingress-cert" "korifi-api-system" "*.$CLUSTER_NAME.korifi.cf-app.com"
 
-  kubectl kustomize "korifi-ci/build/overlays/kpack-image-builder/$CLUSTER_NAME" | kbld -f "korifi-ci/build/kbld/$CLUSTER_NAME/korifi-kpack-image-builder-kbld.yml" -f- | kapp deploy -y -a korifi-kpack-image-builder -f-
+    kubectl kustomize "../korifi-ci/build/overlays/kpack-image-builder/$CLUSTER_NAME" | kbld -f "../korifi-ci/build/kbld/$CLUSTER_NAME/korifi-kpack-image-builder-kbld.yml" -f- | kapp deploy -y -a korifi-kpack-image-builder -f-
 
-  kubectl kustomize "korifi-ci/build/overlays/statefulset-runner/default" | kbld -f "korifi-ci/build/kbld/$CLUSTER_NAME/korifi-statefulset-runner-kbld.yml" -f- | kapp deploy -y -a korifi-statefulset-runner -f-
+    kubectl kustomize "../korifi-ci/build/overlays/statefulset-runner/default" | kbld -f "../korifi-ci/build/kbld/$CLUSTER_NAME/korifi-statefulset-runner-kbld.yml" -f- | kapp deploy -y -a korifi-statefulset-runner -f-
 
-  sed 's/vcap\.me/'$CLUSTER_NAME.korifi.cf-app.com'/' controllers/config/samples/cfdomain.yaml | kubectl apply -f-
+    sed 's/vcap\.me/'$CLUSTER_NAME.korifi.cf-app.com'/' controllers/config/samples/cfdomain.yaml | kubectl apply -f-
+  }
+  popd
+
 }
 
 main() {
