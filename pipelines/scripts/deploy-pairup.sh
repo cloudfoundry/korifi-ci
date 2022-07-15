5c08ae5521c0b8855fcaf911bb684460d487360a
Kieron Browne <kbrowne@vmware.com>
2022-07-15 14:45:23 +0100

    Add deploy pairup step

---
+++ pipelines/scripts/deploy-pairup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source korifi-ci/pipelines/scripts/common/gcloud-functions
+
+gcloud-login
+export-kubeconfig "$CLUSTER_NAME"
+
+kubectl config set-credentials "$SMOKE_TEST_USER" \
+  --client-certificate=<(base64 -d <<<"$CF_ADMIN_CERT") \
+  --client-key=<(base64 -d <<<"$CF_ADMIN_KEY") \
+  --embed-certs
+
+cf api "$CF_API_URL"
+echo cf-admin | cf login
+
+cf create-org pairup || true
+cf create-space -o pairup pairup || true
+cf target -o pairup -s pairup
+
+pushd pairup
+{
+  echo "$FIREBASE_CONF" >src/conf.js
+
+  yarn install
+  yarn build
+
+  cf push pairup
+
+  # once we can do fancy domain names...
+  # cf push pairup --no-start
+  # cf share-private-domain pairup eirini.cf
+  # cf map-route pairup eirini.cf --hostname pairup
+  # cf start pairup
+}
+popd
