4a3e1823d8c2e697f32a0cd5da4bfb3afdb80d8c
Kieron Browne <kbrowne@vmware.com>
2022-07-18 15:46:40 +0100

    try removing the gemfile.lock

--- pipelines/scripts/deploy-postfacto.sh
+++ pipelines/scripts/deploy-postfacto.sh
@@ -38,8 +38,9 @@ applications:
 EOF
 
 sed -i "34i gem 'mini_racer'" postfacto/package/assets/Gemfile
-sed -i "328i \ \ x86_64-linux" postfacto/package/assets/Gemfile.lock
 sed -i "s/ruby '2.7.3'/ruby '2.7.5'/" postfacto/package/assets/Gemfile
+# sed -i "328i \ \ x86_64-linux" postfacto/package/assets/Gemfile.lock
+rm postfacto/package/assets/Gemfile.lock
 
 cf push -f manifest.yml \
   -p postfacto/package/assets \
