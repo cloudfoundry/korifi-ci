e134dc0ac698b1f21fef76470a303d205f518c04
Dave Walter <walterda@vmware.com>
2022-08-08 14:19:34 -0700

    Add korifi-job-task-runner to CI tasks
    Co-authored-by: Matt Royal <mroyal@vmware.com>

--- pipelines/scripts/publish-dev-images.sh
+++ pipelines/scripts/publish-dev-images.sh
@@ -17,6 +17,7 @@ generate_kube_config() {
 update_config_with_version() {
   yq -i ".destinations[0].tags=[\"$VERSION\"]" "$KBLD_CONFIG_DIR/korifi-api-kbld.yml"
   yq -i ".destinations[0].tags=[\"$VERSION\"]" "$KBLD_CONFIG_DIR/korifi-controllers-kbld.yml"
+  yq -i ".destinations[0].tags=[\"$VERSION\"]" "$KBLD_CONFIG_DIR/korifi-job-task-runner-kbld.yml"
   yq -i ".destinations[0].tags=[\"$VERSION\"]" "$KBLD_CONFIG_DIR/korifi-kpack-image-builder-kbld.yml"
   yq -i ".destinations[0].tags=[\"$VERSION\"]" "$KBLD_CONFIG_DIR/korifi-statefulset-runner-kbld.yml"
 }
@@ -26,6 +27,7 @@ publish_images() {
   {
     build-korifi-api >/dev/null
     build-korifi-controllers >/dev/null
+    build-korifi-job-task-runner >/dev/null
     build-korifi-kpack-image-builder >/dev/null
     build-korifi-statefulset-runner >/dev/null
 
