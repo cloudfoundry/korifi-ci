8edbbfba7445924d7667e747dc9aa51956700d41
Kieron Browne <kbrowne@vmware.com>
2022-08-31 11:58:08 +0000

    Add job to bump tests/vendor vendir deps
    Also allow korifi-bot to run e2e tests
    
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- pipelines/scripts/allow-e2e.sh
+++ pipelines/scripts/allow-e2e.sh
@@ -9,7 +9,7 @@ PR_AUTHOR="$(<pr-label/.git/resource/author)"
 team="$(curl -su ${KORIFI_BOT_NAME}:${KORIFI_BOT_TOKEN} ${API_URL}/orgs/${GITHUB_ORG}/teams/${GITHUB_TEAM}/members | jq -r '.[].login')"
 
 for user in $team; do
-  if [[ "$user" == "${PR_AUTHOR}" ]] || [[ "${PR_AUTHOR}" == "dependabot[bot]" ]]; then
+  if [[ "$user" == "${PR_AUTHOR}" ]] || [[ "${PR_AUTHOR}" == "dependabot[bot]" ]] || [[ "${PR_AUTHOR}" == "korifi-bot" ]]; then
     echo Allowing e2e run for trusted user "$user"
     # note that PR labels are updated using the issues endpoint
     curl -XPOST \
---
+++ pipelines/scripts/bump-tests-vendor.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cd korifi/tests
+
+vendir sync
+
+if [[ ! $(git status --porcelain) ]]; then
+  echo "All vendir dependencies already up to date"
+  exit 0
+fi
+
+branch="bump-vendir-$(date +%s)"
+git checkout -b "$branch"
+git add .
+git config user.email "cloudfoundry-korifi@groups.vmware.com"
+git config user.name "Korifi-Bot"
+git commit -m 'Updating vendir dependencies'
+git push origin "$branch"
+
+curl \
+  -X POST \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github+json" \
+  https://api.github.com/repos/cloudfoundry/korifi/pulls \
+  --data '{"title":"Updating vendir dependencies","body":"Generated from korifi CI","head":"'"$branch"'","base":"main"}'
