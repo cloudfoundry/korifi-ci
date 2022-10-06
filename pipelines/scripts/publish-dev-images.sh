424d0bf7d52469832884b389cba59e75af4d6ef9
Danail Branekov <danailster@gmail.com>
2022-10-06 12:15:14 +0300

    Fix reference to renamed overlays directory
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- pipelines/scripts/publish-dev-images.sh
+++ pipelines/scripts/publish-dev-images.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 KBLD_CONFIG_DIR="$PWD/korifi-ci/build/kbld/release"
-VALUES_BASE="$PWD/korifi-ci/build/overlays/acceptance"
+VALUES_BASE="$PWD/korifi-ci/build/values/acceptance"
 COMMIT_SHA=$(cat korifi/.git/ref)
 VERSION="dev-$(cat korifi-release-version/version)-$COMMIT_SHA"
 
