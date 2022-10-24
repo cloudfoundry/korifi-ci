61f93ae3d8759580eebf494395829e3eab25d17d
Giuseppe Capizzi <gcapizzi@vmware.com>
2022-10-24 14:11:22 +0000

    Fix create-release to include the correct docs

--- pipelines/scripts/create-release.sh
+++ pipelines/scripts/create-release.sh
@@ -29,7 +29,8 @@ create_release() {
     export VALUES_BASE=helm/korifi
     build-korifi >"$RELEASE_ARTIFACTS_DIR/korifi/values.yaml"
     cp INSTALL.md "$RELEASE_ARTIFACTS_DIR"
-    cp INSTALL_kind.md "$RELEASE_ARTIFACTS_DIR"
+    cp INSTALL.kind.md "$RELEASE_ARTIFACTS_DIR"
+    cp README.helm.md "$RELEASE_ARTIFACTS_DIR"
   }
   popd
 
