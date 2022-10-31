c0fc2544a52ad44f3a10ccf5bafb4572fe055299
Georgi Sabev <georgethebeatle@gmail.com>
2022-10-31 13:15:45 +0000

    Deploy latest release after tests have passed
    This way next PR test run will test the upgrade from last release to
    latest korifi version and will catch breaking changes early.
    
    Co-authored-by: Danail Branekov <danailster@gmail.com>

---
+++ pipelines/scripts/deploy-released-korifi.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source korifi-ci/pipelines/scripts/common/secrets.sh
+
+VERSION=$(cat korifi-release-version/version)
+
+tmp="$(mktemp -d)"
+trap "rm -rf $tmp" EXIT
+
+deploy() {
+  cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  labels:
+    pod-security.kubernetes.io/enforce: restricted
+  name: korifi
+EOF
+
+  helm upgrade --install korifi \
+    "https://github.com/cloudfoundry/korifi/releases/download/v${VERSION}/korifi-${VERSION}.tgz" \
+    --namespace korifi \
+    --values "../korifi-ci/build/values/$CLUSTER_NAME/values.yaml" \
+    --wait
+
+  if [[ -n "$USE_LETSENCRYPT" ]]; then
+    clone_letsencrypt_cert "korifi-api-ingress-cert" "korifi"
+    clone_letsencrypt_cert "korifi-workloads-ingress-cert" "korifi"
+  fi
+}
+
+create_root_namespace() {
+  cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  labels:
+    pod-security.kubernetes.io/audit: restricted
+    pod-security.kubernetes.io/enforce: restricted
+  name: cf
+EOF
+
+  kubectl delete secret -n cf image-registry-credentials --ignore-not-found
+  kubectl create secret -n cf docker-registry image-registry-credentials \
+    --docker-server="${DOCKER_SERVER}" \
+    --docker-username="${DOCKER_USERNAME}" \
+    --docker-password="${DOCKER_PASSWORD}"
+}
+
+main() {
+  export KUBECONFIG=$PWD/kube/kube.config
+  create_root_namespace
+  deploy
+}
+
+main
