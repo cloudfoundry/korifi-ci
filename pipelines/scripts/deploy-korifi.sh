ec95ec8a6049bf8bae7bf13d2a30bc657268a261
Georgi Sabev <georgethebeatle@gmail.com>
2022-10-31 10:12:40 +0000

    Split pr-e2e values file into image and non-image one
    Thus we would be able to use the non-image file to install a released
    Korifi, i.e. would not override release images
    
    Issue: https://github.com/cloudfoundry/korifi/issues/1847
    
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- pipelines/scripts/deploy-korifi.sh
+++ pipelines/scripts/deploy-korifi.sh
@@ -27,7 +27,7 @@ deploy() {
   {
     kbld \
       -f "../korifi-ci/build/kbld/$CLUSTER_NAME/korifi-kbld.yml" \
-      -f "../korifi-ci/build/values/$CLUSTER_NAME/values.yaml" \
+      -f "../korifi-ci/build/values/$CLUSTER_NAME/image-values.yaml" \
       --images-annotation=false >"$tmp/values.yaml"
 
     helm dependency update helm/korifi
@@ -44,6 +44,7 @@ EOF
     helm upgrade --install korifi helm/korifi \
       --namespace korifi \
       --values "$tmp/values.yaml" \
+      --values "../korifi-ci/build/values/$CLUSTER_NAME/values.yaml" \
       --wait
 
     if [[ -n "$USE_LETSENCRYPT" ]]; then
