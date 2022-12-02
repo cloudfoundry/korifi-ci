b9cead097ee75cfbf0e811f41484b3c186c4647f
Kieron Browne <kbrowne@vmware.com>
2022-12-02 14:02:20 +0000

    Get tokens for cf users when using EKS

--- pipelines/scripts/set-account-env-vars.sh
+++ pipelines/scripts/set-account-env-vars.sh
@@ -8,15 +8,41 @@ main() {
   export KUBECONFIG=$PWD/kube.config
   export-kubeconfig
 
+  case "$CLUSTER_NAME" in
+    "EKS")
+
+      terraform -chdir="cf-k8s-secrets/ci-deployment/$CLUSTER_NAME" init -backend-config="prefix=terraform/state/$CLUSTER_NAME" -upgrade=true
+
+      local CF_ADMIN_KEY_ID CF_ADMIN_SECRET CF_ADMIN_TOKEN
+      CF_ADMIN_KEY_ID="$(terraform -chdir="cf-k8s-secrets/ci-deployment/$CLUSTER_NAME" output -raw cf_admin_key_id)"
+      CF_ADMIN_SECRET="$(terraform -chdir="cf-k8s-secrets/ci-deployment/$CLUSTER_NAME" output -raw cf_admin_secret)"
+      CF_ADMIN_TOKEN="$(AWS_ACCESS_KEY_ID="$CF_ADMIN_KEY_ID" AWS_SECRET_ACCESS_KEY="$CF_ADMIN_SECRET" aws --region "$AWS_REGION" eks get-token --cluster-name "$CLUSTER_NAME")"
+
+      local CF_USER_KEY_ID CF_USER_SECRET CF_USER_TOKEN
+      CF_USER_KEY_ID="$(terraform -chdir="cf-k8s-secrets/ci-deployment/$CLUSTER_NAME" output -raw cf_admin_key_id)"
+      CF_USER_SECRET="$(terraform -chdir="cf-k8s-secrets/ci-deployment/$CLUSTER_NAME" output -raw cf_admin_secret)"
+      CF_USER_TOKEN="$(AWS_ACCESS_KEY_ID="$CF_USER_KEY_ID" AWS_SECRET_ACCESS_KEY="$CF_USER_SECRET" aws --region "$AWS_REGION" eks get-token --cluster-name "$CLUSTER_NAME")"
+
+      # make account-creation.sh skip creating users
+      export E2E_USER_NAME=cf-user
+      export E2E_LONGCERT_USER_NAME=ignore
+      export CF_ADMIN_CERT=ignore
+      export E2E_USER_TOKEN="$CF_USER_TOKEN"
+      export CF_ADMIN_TOKEN
+      ;;
+  esac
+
   source ./korifi/scripts/account-creation.sh $PWD/korifi/scripts
 
   cat <<EOF >accounts/env_vars.yaml
 E2E_USER_NAME: $E2E_USER_NAME
-E2E_USER_PEM: $E2E_USER_PEM
+E2E_USER_PEM: ${E2E_USER_PEM:-}
+E2E_USER_TOKEN: ${E2E_USER_TOKEN:-}
 E2E_SERVICE_ACCOUNT: $E2E_SERVICE_ACCOUNT
 E2E_SERVICE_ACCOUNT_TOKEN: $E2E_SERVICE_ACCOUNT_TOKEN
-CF_ADMIN_KEY: $CF_ADMIN_KEY
-CF_ADMIN_CERT: $CF_ADMIN_CERT
+CF_ADMIN_KEY: ${CF_ADMIN_KEY:-}
+CF_ADMIN_CERT: ${CF_ADMIN_CERT:-}
+CF_ADMIN_TOKEN: ${CF_ADMIN_TOKEN:-}
 EOF
 }
 
