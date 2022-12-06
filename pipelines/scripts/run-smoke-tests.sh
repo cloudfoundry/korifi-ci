1c05d5ccfa89769c822176704f3efec40c74441f
Danail Branekov <danailster@gmail.com>
2022-12-06 16:10:25 +0000

    More fixes
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- pipelines/scripts/run-smoke-tests.sh
+++ pipelines/scripts/run-smoke-tests.sh
@@ -4,10 +4,23 @@ set -euo pipefail
 
 KUBECONFIG="$(realpath "$KUBECONFIG")"
 
-kubectl config set-credentials "$SMOKE_TEST_USER" \
-  --client-certificate=<(base64 -d <<<"$CF_ADMIN_CERT") \
-  --client-key=<(base64 -d <<<"$CF_ADMIN_KEY") \
-  --embed-certs
+case "$CLUSTER_TYPE" in
+  "GKE")
+    kubectl config set-credentials "$SMOKE_TEST_USER" \
+      --client-certificate=<(base64 -d <<<"$CF_ADMIN_CERT") \
+      --client-key=<(base64 -d <<<"$CF_ADMIN_KEY") \
+      --embed-certs
+    ;;
+  "EKS")
+    kubectl config set-credentials "$SMOKE_TEST_USER" \
+      --token=<(base64 -d <<<"$CF_ADMIN_TOKEN")
+    ;;
+
+  *)
+    echo "unknown 'CLUSTER_TYPE': $CLUSTER_TYPE"
+    exit 1
+    ;;
+esac
 
 cd korifi
 ginkgo ./tests/smoke
