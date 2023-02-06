fd6dec39465ade4df9ccc07dbea734752f6a4d3e
Danail Branekov <danailster@gmail.com>
2023-02-06 15:48:24 +0000

    Testing - add a commit
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- pipelines/scripts/create-mel-c.sh
+++ pipelines/scripts/create-mel-c.sh
@@ -5,5 +5,9 @@ set -euo pipefail
 pushd cf-k8s-secrets/bosh-lite-cf/directors
 {
   date >testing
+  git add .
+  git config user.name "Korifi Bot"
+  git config user.email "korifi@cloudfoundry.org"
+  git commit -m "saving mel-c state"
 }
 popd
