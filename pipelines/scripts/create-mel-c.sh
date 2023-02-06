5ce3727b1a2841c9f6d060e497e34b0247ac992d
Danail Branekov <danailster@gmail.com>
2023-02-06 15:54:11 +0000

    Change bot username
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- pipelines/scripts/create-mel-c.sh
+++ pipelines/scripts/create-mel-c.sh
@@ -7,7 +7,7 @@ pushd cf-k8s-secrets/bosh-lite-cf/directors
   date >testing
   git add .
   git config user.name "Korifi Bot"
-  git config user.email "korifi@cloudfoundry.org"
+  git config user.email "cloudfoundry-korifi@groups.vmware.com"
   git commit -m "saving mel-c state"
 }
 popd
