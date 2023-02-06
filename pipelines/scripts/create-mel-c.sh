98db69c80637e7fb7983eb177c46336d3dcd5af8
Danail Branekov <danailster@gmail.com>
2023-02-06 17:07:22 +0000

    Don't create mel-c if it already exists
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- pipelines/scripts/create-mel-c.sh
+++ pipelines/scripts/create-mel-c.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+if curl -fSsLko /dev/null --connect-timeout 2 https://api.mel-c.korifi.cf-app.com; then
+  echo "mel-c CF already exists"
+  exit 0
+fi
+
 pushd cf-k8s-secrets/bosh-lite-cf/directors
 {
   ./lite-me-up.sh create mel-c
--- pipelines/scripts/save-mel-c-state.sh
+++ pipelines/scripts/save-mel-c-state.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 
 pushd cf-k8s-secrets/bosh-lite-cf/directors
 {
+  if [[ "$(git status -s)" == "" ]]; then
+    echo "No change"
+    exit 0
+  fi
+
   git add .
   git config user.name "Korifi Bot"
   git config user.email "cloudfoundry-korifi@groups.vmware.com"
