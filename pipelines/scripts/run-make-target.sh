32bcc55e9f8466543a2ba367c30bfbed04c815be
Giuseppe Capizzi <gcapizzi@vmware.com>
2023-03-10 15:00:17 +0000

    Expose registry cred list to run-make-target task
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- pipelines/scripts/run-make-target.sh
+++ pipelines/scripts/run-make-target.sh
@@ -2,6 +2,25 @@
 
 set -euo pipefail
 
+# for multi-registry image testing (tools/image)
+export REGISTRY_DETAILS
+REGISTRY_DETAILS="
+- server: europe-docker.pkg.dev
+  pathPrefix: europe-docker.pkg.dev/cf-on-k8s-wg/test
+  username: _json_key_base64
+  password: $(base64 -w0 <<<"$GCP_ARTIFACT_WRITER_KEY")
+
+- server: 007801690126.dkr.ecr.eu-west-1.amazonaws.com
+  repoName: 007801690126.dkr.ecr.eu-west-1.amazonaws.com/test
+  username: AWS
+  password: $(aws ecr get-login-password --region eu-west-1)
+
+- server: korifitest.azurecr.io
+  pathPrefix: korifitest.azurecr.io/test
+  username: $ACR_USER
+  password: $ACR_PASSWORD
+"
+
 make -C "root/$DIR" "$TARGET"
 
 pushd root
