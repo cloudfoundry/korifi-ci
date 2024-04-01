b17fea1f534170491919254088a9e9a62723d868
Danail Branekov <danailster@gmail.com>
2024-04-01 11:45:24 +0000

    Deploy spring-music on acceptance
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

---
+++ pipelines/scripts/deploy-spring-music.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source korifi-ci/pipelines/scripts/common/target.sh
+
+cf create-org spring-music
+cf create-space -o spring-music spring-music
+cf target -o spring-music -s spring-music
+
+cf push -f spring-music/manifest.yml \
+  -p spring-music \
+  --var SPRING_PROFILES_ACTIVE=postgres \
+  --no-start
+
+db_uri="postgres://$DB_USERNAME:$DB_PASSWORD@$DB_ADDR:5432/spring-music"
+cf create-user-provided-service spring-music-database -p "{\"uri\": \"${db_uri}\"}"
+cf bind-service spring-music spring-music-database
+
+cf start spring-music
