5f371afde07e8f1a9c41a4dec4a327e973008298
Danail Branekov <danailster@gmail.com>
2024-04-09 09:39:05 +0000

    Configure default app bits for crd tests
    Co-authored-by: Georgi Sabev <georgethebeatle@gmail.com>

--- pipelines/scripts/run-crds-tests.sh
+++ pipelines/scripts/run-crds-tests.sh
@@ -6,4 +6,5 @@ source korifi-ci/pipelines/scripts/common/target.sh
 source korifi-ci/pipelines/scripts/common/run-with-timeout.sh
 
 export SKIP_DEPLOY=true
+export DEFAULT_APP_BITS_PATH=$(readlink -f "$DEFAULT_APP_BITS_PATH")
 run-with-timeout make -C korifi test-crds
