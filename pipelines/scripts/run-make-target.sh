b5f948627ca021971ee54d7a7d7cf8c60e2b2ec4
Georgi Sabev <georgethebeatle@gmail.com>
2025-09-17 12:32:56 +0000

    Do not publish code-climate reports
    code-climate is no longer alive, it has been replaced by qlty.sh.
    Migration is not trivial, furthermore qlty does not seem to support
    gocov coverage format. Therefore, we stop publishing coverage reports.
    
    issue https://github.com/cloudfoundry/korifi/issues/4164
    
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- pipelines/scripts/run-make-target.sh
+++ pipelines/scripts/run-make-target.sh
@@ -17,13 +17,3 @@ REGISTRY_DETAILS="
 "
 
 make -C "root/$DIR" "$TARGET"
-
-pushd root
-{
-  idx=0
-  for c in $(find "$DIR" -name cover.out); do
-    cc-test-reporter format-coverage $c -t gocov -o ../coverage/$idx.codeclimate.json -p code.cloudfoundry.org/korifi
-    idx=$((idx + 1))
-  done
-}
-popd
